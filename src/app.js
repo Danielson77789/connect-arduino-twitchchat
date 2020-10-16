59d1baf03c0be5722dee6f02612a817c53575aa7
Daniel <Djp77789@gmail.com>
2020-10-15 20:37:36 -0700

    Added comments

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import tmi from "tmi.js";
 var jFive = require("johnny-five");
 let startPos = 0;
+
+// Only needed if having issues with COM port
 var board = new jFive.Board({
   port: "COM3"
 });
@@ -19,42 +21,19 @@ const client = new tmi.Client({
 });
 
 board.on("ready", function() {
-    var led = new jFive.Led(9);
-    var led2 = new jFive.Led(8);
-    //led.blink(500);
     client.connect();
-    //let switchLight = false;
-    //let speed = 500;
-    //const followServo = new jFive.Servo({
-    //    pin: 9,
-    //    startAt:0
-    //});
-
-    function Blink() {
-        led.on();
-        led.off();
-        led2.on();
-        led2.off();
-    };
 
     client.on('message', (channel, userstate, message, self) => {
         
         if(self) return;
-        //followServo.to(180);
-        //followServo.to(0);
+
 
         if(message.toLowerCase() === '!slow') {
-            Blink();
-            //if (startPos >= 5){
-            //followServo.to(startPos);
-            //startPos -= 5;
-            //};
+            //Do something when !slow is typed in twitch chat
+
         } else if (message.toLowerCase() === '!fast') {
             led.off();
-            //if (startPos <= 180){
-            //followServo.to(startPos);
-            //startPos += 5;
-            //};
+
         }
     
     });
